a17e2deee2831de748d3d5c5311e6fc3b31920e0
Juan Camilo Ramirez <j142857z@gmail.com>
2016-05-24 09:46:28 -0500

    2016-05-13 11:35:00

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clc;
 clear;
 
 %Modify the paths for your computer
-path = '/home/jramirez/Documents/Dynamics4GenomicBigData/';
+path = '/home/jramirez/Dynamics4GenomicBigData/';
 %path = '/Users/michellecarey/Dropbox/Dynamics4GenomicBigData/';
 
 %if first time running on computerrun line 9 and 10 
@@ -23,6 +23,9 @@ for i = 1:size(GEO_num,1)
 %User Inputs/options
 GEO_number              = char(GEO_num(i));
 Preprocessing_technique = 'Default';
+%  Preprocessing_technique = 'RMA';
+%  Preprocessing_technique = 'MAS5';
+%  Preprocessing_technique = 'GCRMA';
 
 % -----------------------------------------------------------------------
 %  Data Preprocessing
