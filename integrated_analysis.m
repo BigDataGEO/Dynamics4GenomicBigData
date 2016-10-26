9bba54345830ecea0327f419a82ac271312f2c85
Juan Camilo Ramirez <j142857z@gmail.com>
2016-10-26 14:25:38 -0500

    Integrated analsysis now outputs common DRGs between conditions.

--- integrated_analysis.m
+++ integrated_analysis.m
@@ -40,36 +40,6 @@ function integrated_analysis()
   writetable(cell2table(statistics_of_analyses), 'Comparison.csv', 'WriteVariableNames', false);
   
   cd(Dynamics4GenomicBigData_HOME);
-  
-%    for i = 1:size(inputData,1) 
-%       
-%      % One to one analyses, that are perfomed only between conditions belonging to the same series.
-%      for j = 1:size(inputData,1)
-%        if (i~=j)
-%        
-%  	% Get the condition's analysis data.
-%  	[gene_expression_1, time_points_1, list_of_DRGs_1, list_of_gene_clusters_1, gene_expression_by_cluster_1, list_of_cluster_means_1, coefficients_1, adjacency_matrix_of_gene_regulatory_network_1, network_graph_1, graph_statistics_1, node_statistics_1, subject_name_1, gene_ID_type_1, indices_of_DRGs_1] = load_analysis(inputData{i,1}, inputData{i,2});
-%      
-%  	% Get the second condition's analysis data.
-%  	[gene_expression_2, time_points_2, list_of_DRGs_2, list_of_gene_clusters_2, gene_expression_by_cluster_2, list_of_cluster_means_2, coefficients_2, adjacency_matrix_of_gene_regulatory_network_2, network_graph_2, graph_statistics_2, node_statistics_2, subject_name_2, gene_ID_type_2, indices_of_DRGs_2] = load_analysis(inputData{j,1}, inputData{j,2});
-%  	
-%  	one_to_one_comparison_folder = [general_comparison_folder, '/', inputData{i,1}, '_vs_', inputData{j,1} '/', inputData{i,2}, '_vs_', inputData{j,2}];
-%  	
-%  	if(strcmp(inputData{i,1}, inputData{j,1}))
-%  	  one_to_one_comparison_folder = [general_comparison_folder, '/', inputData{i,1}, '/', inputData{i,2}, '_vs_', inputData{j,2}];
-%  	  
-%  	  mkdir(one_to_one_comparison_folder);
-%  	  cd(one_to_one_comparison_folder);
-%  	  
-%  	  output_comparison_plots(subject_name_1, list_of_gene_clusters_1, gene_expression_by_cluster_1, list_of_cluster_means_1, time_points_1, subject_name_2, zscore(gene_expression_2')', indices_of_DRGs_1);
-%  
-%  	  plot_cluster_matches(subject_name_1, gene_expression_by_cluster_1, list_of_cluster_means_1, time_points_1, subject_name_2, gene_expression_by_cluster_2, list_of_cluster_means_2, time_points_2);
-%  	  
-%  	  cd(Dynamics4GenomicBigData_HOME);
-%  	end
-%        end
-%      end
-%    end
  
 end
 
