63cc4ad3b69dcb8f5eccd4ddd98e1d304e494fef
Juan Camilo Ramirez <j142857z@gmail.com>
2016-11-02 10:34:25 -0500

    Common DRGs across conditions are determined more efficiently.

--- integrated_analysis.m
+++ integrated_analysis.m
@@ -31,95 +31,38 @@ function integrated_analysis()
     GRMs{i} = list_of_gene_clusters;    
   end
   
-  common_genes =  get_common_genes_across_conditions(DRGs);
-  list_of_conditions = inputData(:,2)';
+  frequency_of_DRGs =  get_frequency_of_DRGs(DRGs);
   
   cd(general_comparison_folder);
   
-  output_common_genes_reports(common_genes, list_of_conditions);
+  writetable(cell2table(frequency_of_DRGs), 'Frequency_of_DRGs.csv', 'WriteVariableNames', false);
   writetable(cell2table(statistics_of_analyses), 'Comparison.csv', 'WriteVariableNames', false);
   
   cd(Dynamics4GenomicBigData_HOME);
  
 end
 
-function output_common_genes_reports(common_genes, list_of_conditions)
+function frequency_of_DRGs =  get_frequency_of_DRGs(DRGs)
   
-  for size_of_output_combinations = 1:length(list_of_conditions)
-    output_common_genes_report_for_one_combination(common_genes, size_of_output_combinations, list_of_conditions)
-  end
+  all_genes_with_repetition = [];
   
-end
-
-function output_common_genes_report_for_one_combination(common_genes, size_of_output_combinations, list_of_conditions)
-  output_matrix_by_combination_size = [repmat({'Condition'}, 1, size_of_output_combinations) {'#of DRGs in Common'} {'DRG file'}];
-  
-  common_drgs_folder = 'common_DRGs';
-  mkdir(common_drgs_folder);
-  cd(common_drgs_folder);
-  
-  for combination_of_conditions_index = 1:size(common_genes,1)
-    combination_of_conditions = common_genes(combination_of_conditions_index,:);
-    
-    if(length(combination_of_conditions{1}) == size_of_output_combinations)
-      conditions = combination_of_conditions{1};
-      common_drgs = combination_of_conditions{2};
-      common_drgs_file = [num2str(combination_of_conditions_index) '.csv'];
-      output_matrix_by_combination_size = [output_matrix_by_combination_size; list_of_conditions(conditions) {num2str(length(common_drgs))} {common_drgs_file}];
-      writetable(cell2table(common_drgs'), common_drgs_file, 'WriteVariableNames', false)
-    end
-  end
-  
-  cd('..');
-  writetable(cell2table(output_matrix_by_combination_size), ['Common_genes_' num2str(size_of_output_combinations) '.csv'], 'WriteVariableNames', false);
-end
-
-function common_genes =  get_common_genes_across_conditions(DRGs)
-
   n = size(DRGs,2);
-
-  % all_combinations_of_condition_indices{1} is the list of size-1 combinations of condition indices.
-  all_combinations_of_condition_indices = [];
-  for k=1:n
-    all_combinations_of_condition_indices = [all_combinations_of_condition_indices; {combnk(1:n,k)}];
-  end
   
-  common_genes = [];
-  
-  % n is the number of conditions
-  % k is the  combination size.
-  % C(n,k) is the number of k-sized combinations from n n-sized set.
   for k=1:n
-    k_sized_combinations = all_combinations_of_condition_indices{k};
-    
-    for combination_index = 1:size(k_sized_combinations,1)
-      combination = k_sized_combinations(combination_index,:);      
-      sets = [];
-      
-      for combination_member_index = 1:length(combination)
-	combination_member = combination(combination_member_index);	
-	genes_in_current_condition = unique(DRGs{combination_member})';	
-	sets = [sets; {genes_in_current_condition}];	
-      end
-      
-      intersection = get_intersection(sets);      
-      common_genes = [common_genes; {combination} {intersection}];      
-    end    
+    all_genes_with_repetition = [all_genes_with_repetition; unique(DRGs{k})];
   end
+ 
+  frequency_of_DRGs = get_frequency_of_each_array_element(all_genes_with_repetition);
+  
 end
 
+function frequency_per_element = get_frequency_of_each_array_element(the_array)
 
-% This function returns the intersection of all the rows in sets. That is to say, each row is a set. Each row is a cell array.
-
-% Example input.
-
-% sets = {[1, 2, 3]; [5 9 3 6 96 2]; [2 96 3]}
-function intersection = get_intersection(sets)
-
-  intersection = sets{1,:};
+  [a b c] = unique(the_array);
+  d = hist(c,length(a));
+  P = [a num2cell(d')];
   
-  for row_index = 2:size(sets,1)
-    intersection = intersect(intersection, sets{row_index,:});
-  end
-
+  [B I] = sort(cell2mat(P(:,2)), 'descend');
+  
+  frequency_per_element = P(I,:);  
 end
--- manual.m
+++ manual.m
@@ -1,20 +1,20 @@
 set_paths_and_imports;
 
-GEO_number = 'GSE46045';
+GEO_number = 'GSE52428';
 
 % The following function call may take some time to complete.
 [geoStruct, list_of_genes, gene_ID_type] = get_geo_data(GEO_number);
 
 [raw_gene_expression, raw_time_points, subject_name, condition, number_of_top_DRGs_considered] = step_1(geoStruct);
 
-[gene_expression, time_points, smooth_gene_trajectories] = step_2(raw_gene_expression, raw_time_points, true);
+[gene_expression, time_points, smooth_gene_trajectories] = step_2(raw_gene_expression, raw_time_points, false);
 
-[list_of_genes_sorted_by_F_value, gene_expression_sorted_by_F_value, number_of_statistically_significant_DRGs, smooth_gene_expression, fd_smooth_coefficients, indices_of_DRGs, list_of_DRGs] = step_3(list_of_genes, gene_expression, time_points, smooth_gene_trajectories, number_of_top_DRGs_considered, true);
+[list_of_genes_sorted_by_F_value, gene_expression_sorted_by_F_value, number_of_statistically_significant_DRGs, smooth_gene_expression, fd_smooth_coefficients, indices_of_DRGs, list_of_DRGs] = step_3(list_of_genes, gene_expression, time_points, smooth_gene_trajectories, number_of_top_DRGs_considered, false);
 
-[list_of_gene_clusters, gene_expression_by_cluster, list_of_cluster_means] = step_4(gene_expression, time_points, list_of_DRGs, indices_of_DRGs, smooth_gene_expression, true);
+[list_of_gene_clusters, gene_expression_by_cluster, list_of_cluster_means] = step_4(gene_expression, time_points, list_of_DRGs, indices_of_DRGs, smooth_gene_expression, false);
 
-[coefficients, adjacency_matrix_of_gene_regulatory_network] = step_5(list_of_gene_clusters, time_points, indices_of_DRGs, fd_smooth_coefficients, true);
+[coefficients, adjacency_matrix_of_gene_regulatory_network] = step_5(list_of_gene_clusters, time_points, indices_of_DRGs, fd_smooth_coefficients, false);
 
-[network_graph, graph_statistics, node_statistics] = step_6(adjacency_matrix_of_gene_regulatory_network, true);
+[network_graph, graph_statistics, node_statistics] = step_6(adjacency_matrix_of_gene_regulatory_network, false);
 
 [chartReport, tableReport] = step_7(list_of_genes, list_of_gene_clusters, indices_of_DRGs, gene_ID_type);
