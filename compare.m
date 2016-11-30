c72f75616d0a449f998485c01cf2d723430aa886
Juan Camilo Ramirez <j142857z@gmail.com>
2016-11-30 13:00:02 -0600

    Maintenance

--- compare.m
+++ compare.m
@@ -8,14 +8,18 @@ function compare()
   
   parts = strsplit(input_file_name, '.');
   
-  output_dir = parts{1};
+  parts = strsplit(parts{1}, '_-_');
+  
+%    output_dir = ['Results/' parts{1} '/Comparison/' parts{2}];
+  
+  GEO_number = parts{1};
 
-  inputData = readtable(['Input/' input_file_name], 'Delimiter', ',');
+  inputData = readtable(['Input/' input_file_name], 'Delimiter', ',', 'ReadVariableNames',false);
   inputData = table2cell(inputData);
   
   if(size(inputData,1) > 1)
   
-    general_comparison_folder = [Dynamics4GenomicBigData_HOME, 'Results/' inputData{1,1} '/Comparison/' output_dir];
+    general_comparison_folder = [Dynamics4GenomicBigData_HOME, 'Results/' parts{1} '/Comparison/'];
     mkdir(general_comparison_folder);
     
     for i = 1:size(inputData,1) 
@@ -25,14 +29,14 @@ function compare()
 	if (i~=j)
 	
 	  % Get the condition's analysis data.
-	  [gene_expression_1, time_points_1, list_of_top_DRGs_1, list_of_gene_clusters_1, gene_expression_by_cluster_1, list_of_cluster_means_1, coefficients_1, adjacency_matrix_of_gene_regulatory_network_1, network_graph_1, graph_statistics_1, node_statistics_1, subject_name_1, gene_ID_type_1, indices_of_top_DRGs_1, number_of_statistically_significant_DRGs, list_of_genes, gene_expression_sorted_by_F_value, list_of_probe_ids] = load_analysis(inputData{i,1}, inputData{i,2});
+	  [gene_expression_1, time_points_1, list_of_top_DRGs_1, list_of_gene_clusters_1, gene_expression_by_cluster_1, list_of_cluster_means_1, coefficients_1, adjacency_matrix_of_gene_regulatory_network_1, network_graph_1, graph_statistics_1, node_statistics_1, subject_name_1, gene_ID_type_1, indices_of_top_DRGs_1, number_of_statistically_significant_DRGs, list_of_genes, gene_expression_sorted_by_F_value, list_of_probe_ids] = load_analysis(GEO_number, inputData{i,1});
       
 	  % Get the second condition's analysis data.
-	  [gene_expression_2, time_points_2, list_of_top_DRGs_2, list_of_gene_clusters_2, gene_expression_by_cluster_2, list_of_cluster_means_2, coefficients_2, adjacency_matrix_of_gene_regulatory_network_2, network_graph_2, graph_statistics_2, node_statistics_2, subject_name_2, gene_ID_type_2, indices_of_top_DRGs_2, number_of_statistically_significant_DRGs_2, list_of_genes_2, gene_expression_sorted_by_F_value_2, list_of_probe_ids] = load_analysis(inputData{j,1}, inputData{j,2});
+	  [gene_expression_2, time_points_2, list_of_top_DRGs_2, list_of_gene_clusters_2, gene_expression_by_cluster_2, list_of_cluster_means_2, coefficients_2, adjacency_matrix_of_gene_regulatory_network_2, network_graph_2, graph_statistics_2, node_statistics_2, subject_name_2, gene_ID_type_2, indices_of_top_DRGs_2, number_of_statistically_significant_DRGs_2, list_of_genes_2, gene_expression_sorted_by_F_value_2, list_of_probe_ids] = load_analysis(GEO_number, inputData{j,1});
  
-%  	  one_to_one_comparison_folder = [general_comparison_folder, '/', inputData{i,1}, '_vs_', inputData{j,1} '/', inputData{i,2}, '_vs_', inputData{j,2}];
+%  	  one_to_one_comparison_folder = [general_comparison_folder, '/', GEO_number, '_vs_', GEO_number '/', inputData{i,1}, '_vs_', inputData{j,1}];
 	  
-	  if(strcmp(inputData{i,1}, inputData{j,1}))
+	  if(strcmp(GEO_number, GEO_number))
 	  
 	    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	    
@@ -53,8 +57,8 @@ function compare()
 	    
 	    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	  
-%  	    one_to_one_comparison_folder = [general_comparison_folder, '/', inputData{i,1}, '/', inputData{i,2}, '_vs_', inputData{j,2}];
-	    one_to_one_comparison_folder = [general_comparison_folder, '/', inputData{i,2}, '_vs_', inputData{j,2}];
+%  	    one_to_one_comparison_folder = [general_comparison_folder, '/', GEO_number, '/', inputData{i,1}, '_vs_', inputData{j,1}];
+	    one_to_one_comparison_folder = [general_comparison_folder, '/', inputData{i,1}, '_vs_', inputData{j,1}];
 	    
 	    mkdir(one_to_one_comparison_folder);
 	    cd(one_to_one_comparison_folder);
